4aef770f4f81138c0b715a052ab951475995d68d
Nathan Wood <nafe.wood@gmail.com>
2016-04-05 10:51:58 +0100

    Better error handling around api

--- src/main/java/com/nathan/FruitController.java
+++ src/main/java/com/nathan/FruitController.java
@@ -2,6 +2,10 @@ package com.nathan;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +16,24 @@ public class FruitController
 	
 
 	@RequestMapping("/*")
-	public String getRequest(HttpServletRequest request)
+	public ResponseEntity<String> getRequest(HttpServletRequest request)
 	{
 		String basket = request.getParameter("basket");
 		Basket myBasket = new Basket();
-		myBasket.populateBasket(basket);
-		String response = myBasket.getReceipt();
+		String response;
+		try
+		{
+			myBasket.populateBasket(basket);
+			response = myBasket.getReceipt();	
+		}
+		catch (IllegalArgumentException ae)
+		{
+			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		}
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON_UTF8);		
 				
-		return response;
+		return new ResponseEntity<String>(response,headers,HttpStatus.OK);
 	}
 
 }
--- src/test/java/com/nathan/FruitControllerTest.java
+++ src/test/java/com/nathan/FruitControllerTest.java
@@ -3,22 +3,24 @@ package com.nathan;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import com.google.gson.Gson;
 
 public class FruitControllerTest {
 
-	Gson gson = new Gson();
+	
 	
 	private static double SMALL_NUMBER=0.0000001d;
 	
 	@Test
 	public void testNoQueryString() 
 	{
+		Gson gson = new Gson();
 		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
 		FruitController unit = new FruitController();
-		Basket result = gson.fromJson(unit.getRequest(mockRequest), Basket.class);
+		Basket result = gson.fromJson(unit.getRequest(mockRequest).getBody(), Basket.class);
 		assertEquals(0,result.getTotalCost().doubleValue(),SMALL_NUMBER);
 		
 	}
@@ -26,40 +28,36 @@ public class FruitControllerTest {
 	@Test
 	public void testEmptyString() 
 	{
+		Gson gson = new Gson();
 		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
 		mockRequest.addParameter("basket", "");
 		FruitController unit = new FruitController();
-		Basket result = gson.fromJson(unit.getRequest(mockRequest), Basket.class);
+		Basket result = gson.fromJson(unit.getRequest(mockRequest).getBody(), Basket.class);
 		assertEquals(0,result.getTotalCost().doubleValue(),SMALL_NUMBER);
 	}
 	
 	@Test
 	public void testPopulatedQueryString() 
 	{
+		Gson gson = new Gson();
 		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
 		mockRequest.addParameter("basket", "orange,apple");
 		FruitController unit = new FruitController();
-		Basket result = gson.fromJson(unit.getRequest(mockRequest), Basket.class);
+		Basket result = gson.fromJson(unit.getRequest(mockRequest).getBody(), Basket.class);
 		assertEquals(0.85,result.getTotalCost().doubleValue(),SMALL_NUMBER);
 	}
 	
 	@Test
 	public void testBadInput() 
 	{
+		
 		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
 		mockRequest.addParameter("basket", "orange,apple,coconut");
 		FruitController unit = new FruitController();
 		
-		try
-		{
-			unit.getRequest(mockRequest);
-		}
-		catch (IllegalArgumentException ae)
-		{
-			assertEquals("Can't find a fruit Enum that matches coconut", ae.getMessage());
-			return;
-		}
-		fail("Shouldn't have got this far!");
+		
+		assertEquals(HttpStatus.BAD_REQUEST,unit.getRequest(mockRequest).getStatusCode());
+		
 	}
 
 }
